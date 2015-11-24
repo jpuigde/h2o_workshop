b168c95a1e52ce47942b1083ba5d94f202da307b
jpuigde <jpuigde@gmail.com>
2015-11-24 17:34:15 +0100

    .

--- AWS_h2o_install.sh
+++ AWS_h2o_install.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/bash 
 
 sudo apt-get update
 sudo apt-get install default-jdk -y
 sudo apt-get install unzip
 wget http://h2o-release.s3.amazonaws.com/h2o/rel-tibshirani/3/h2o-3.6.0.3.zip
 unzip h2o-3.6.0.3.zip
-cd h2o-3.6.0.3
+
 
