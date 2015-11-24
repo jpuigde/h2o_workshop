c5837268348232963b0f65a4dbb21f347cd9b60f
jpuigde@gmail.com <jpuigde>
2015-11-24 16:31:07 +0100

    commit

--- AWS_h2o_install.sh
+++ AWS_h2o_install.sh
@@ -4,5 +4,4 @@ sudo apt-get install unzip
 wget http://h2o-release.s3.amazonaws.com/h2o/rel-tibshirani/3/h2o-3.6.0.3.zip
 unzip h2o-3.6.0.3.zip
 cd h2o-3.6.0.3
-wget https://raw.githubusercontent.com/jpuigde/h2o_workshop/master/flatfile.txt
-java -Xmx1g -jar h2o.jar -flatfile flatfile.txt -port 54321
+
